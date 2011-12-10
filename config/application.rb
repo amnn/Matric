2a5f1f2d76bf803343185277e833b7c35c7618f8
Ashok Menon <amenon94@gmail.com>
2011-12-10 22:06:42 +0000

    correction, the same again

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require Rails.root.join("/app/models/matrix")
-require Rails.root.join("/app/models/expression")
-require Rails.root.join("/app/models/calculation")
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
