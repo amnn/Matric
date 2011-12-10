0b6320b26abeb6287a6a5e27410ee5ade3a5b29e
Ashok Menon <amenon94@gmail.com>
2011-12-10 21:34:38 +0000

    Eagerloading enabled

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,14 @@ module Matric
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    config.autoload_paths += %W(#{config.root}/app/models)
+
+    Rails::Application.config.autoload_paths.each do |d|
+      Dir["#{d}/*.rb"].each do |p|
+        require p
+      end
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
